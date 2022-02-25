6378fef276a5a7b471bd6dd6c409da02b46a1485
coffeepenbit <coffeepenbit@gmail.com>
2022-02-25 18:21:31 -0500

    Fix copying wrong settings

--- run_thread_switcher_aida64_max_frequency.bat
+++ run_thread_switcher_aida64_max_frequency.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 
-copy /Y settings_aida64.py settings.py
+copy /Y settings_aida64_max_frequency.py settings.py
 
 :: You may also replace this with `python` or `python3X` if you are having issues
 py main.py
